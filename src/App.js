3ce8fbd445db9f01c9e688d20afbb30d4cf41d0f
tomasdetloging <tomasdetloging@gmail.com>
2020-10-09 08:48:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import useWindowSize from "@rooks/use-window-size";
+import ParticleImage, { Vector, forces } from "react-particle-image";
 
-function App() {
+const particleOptions = {
+  filter: ({ x, y, image }) => {
+    // Get pixel
+    const pixel = image.get(x, y);
+    // Make a particle for this pixel if blue > 50 (range 0-255)
+    return pixel.b > 50;
+  },
+  color: ({ x, y, image }) => "#ffffff",
+  radius: () => Math.random() * 1.5 + 0.5,
+  mass: () => 40,
+  friction: () => 0.15,
+  initialPosition: ({ canvasDimensions }) => {
+    return new Vector(canvasDimensions.width / 2, canvasDimensions.height / 2);
+  },
+};
+
+const motionForce = (x, y) => {
+  return forces.disturbance(x, y, 5);
+};
+
+export default function App() {
+  const { innerWidth, innerHeight } = useWindowSize();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ParticleImage
+      src={require("./react-logo.png")}
+      width={Number(innerWidth)}
+      height={Number(innerHeight)}
+      scale={0.85}
+      entropy={6}
+      maxParticles={3000}
+      particleOptions={particleOptions}
+      mouseMoveForce={motionForce}
+      touchMoveForce={motionForce}
+      // backgroundColor="hsl(207 90% 54% / 0)"
+    />
   );
 }
-
-export default App;
